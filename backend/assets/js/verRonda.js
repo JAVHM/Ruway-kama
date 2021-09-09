adfaf18563de49082edd395132217ab613d0a5c5
JAVHM <55992367+JAVHM@users.noreply.github.com>
2021-09-08 21:33:50 -0500

    1.1

--- backend/assets/js/verRonda.js
+++
@@ -1,9 +0,0 @@
-function alerta() {
-    alert("El ganador ha sido actualizado");
-    console.log("XD?")
-}
-function test(){
-    console.log("test")
-}
-const btn = document.querySelectorAll('.boton_submit');
-btn.forEach(el => el.addEventListener('click', alerta));
