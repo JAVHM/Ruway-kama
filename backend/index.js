9f7feb1e9434cbd2d2df4e935f23c90575796d89
JAVHM <55992367+JAVHM@users.noreply.github.com>
2021-09-28 00:54:53 -0500

    .

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,5 @@ app.get('/', (req, res)=> {
 })
 
 app.listen(PORT, ()=> {
-    console.log(`Servidor ejecutandose en el puerto ${PORT}`)
+    console.log(`Servidor ejecutandose en el puerto ${PORT}`);
 })
