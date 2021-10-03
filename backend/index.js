a52e4b9420bdffc7b401f7ec10a11441fbd75a2a
LeiteQuintanilha <josueleitesan@gmail.com>
2021-10-03 16:40:54 -0500

    Cambios en el index + Views

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.search('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
 app.listen(3000, () => {
     console.log('Servidor funcional en http://localhost:3000')
 });
