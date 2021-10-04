c3097d9db931299a1183a175fcc587ff8995109a
Gian <guianpierodiaz15@gmail.com>
2021-10-04 12:43:38 -0500

    ORM sequelize, views, assets

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.search('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, 'assets')))
 
 app.listen(3000, () => {
     console.log('Servidor funcional en http://localhost:3000')
 });
+
+app.get("/",(req,res)=>{
+    res.render('index');
+})
