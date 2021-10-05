55ab188b0d7088c2f26e700700f063660f3a765c
Gian <guianpierodiaz15@gmail.com>
2021-10-05 01:10:47 -0500

    Match User Proyecto

--- backend/sequelize/models/proyecto.js
+++ backend/sequelize/models/proyecto.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Proyecto.belongsTo(models.Usuario, {
         foreignKey: 'idProyecto'
+        
       })
     }
   };
--- backend/sequelize/models/usuario.js
+++ backend/sequelize/models/usuario.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Usuario.hasMany(models.Proyecto,{
         foreignKey: 'idProyecto'
+        
       })
     }
   };
