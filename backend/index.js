8d123c6c6f3cb4ea2324a847fd5b4dd824d75143
diego <diegoarcm22@gmail.com>
2021-10-05 23:19:41 -0500

    cambio index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const app = express();
 const path = require('path');
 const bcryptjs = require('bcryptjs');
 const { getUsuarios, createUsuario } = require('./models/dao_usuario')
-
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended : true
+}))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(path.join(__dirname, 'assets')))
