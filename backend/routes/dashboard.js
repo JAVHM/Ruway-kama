82bebc8c4dba4870bba2a7492b268d8dba93b337
JAVHM <55992367+JAVHM@users.noreply.github.com>
2021-11-08 15:53:23 -0500

    avance obtener datos de suuario

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getUsuario} = require('../models/dao_usuario')
 
-router.get("/dashboard", async (req, res) => {
-    res.render('dashboard')
+router.get("/dashboard/:id", async (req, res) => {
+    const uId = req.params.id;
+    const u = await getUsuario(parseInt(uId));
+    res.render('dashboard', {
+        usuario: u
+    })
 })
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { getUsuario} = require('../models/dao_usuario')
 
-router.get('/',(req,res)=>{
+router.get('/', async (req,res)=>{
     if(req.session.u_id==null){
         req.session.login= false;
+        res.render('index', {
+            registrado : req.session.login,
+        });
     }else{
         req.session.login= true;
+        usuario = await getUsuario(parseInt(req.session.u_id));
+        console.log("nombre: ", usuario.nombre)
+        res.render('index', {
+            registrado : req.session.login,
+            u : usuario
+        });
     }
-    res.render('index', {
-        registrado : req.session.login
-    });
+    
 })
 module.exports = router;
