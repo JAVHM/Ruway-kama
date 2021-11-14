bca0f9a84050f0d03c2179a91bf291d19651e046
diego <diegoarcm22@gmail.com>
2021-11-13 19:44:44 -0500

    cree seeder moderador

---
+++ backend/sequelize/seeders/20211113225104-moderador.js
@@ -0,0 +1,37 @@
+'use strict';
+const bcrypt = require("bcryptjs")
+const salt = bcrypt.genSaltSync(10)
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   // Agregar usuarios 
+   return queryInterface.bulkInsert('Usuario', [
+    // agregar administrador 
+    {
+      nombre: 'administrador',
+      contraseña: bcrypt.hashSync("admin", salt),
+      correo: 'administrador@example.com',
+      descripcion: "Este es la cuenta del moderador",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     return queryInterface.bulkDelete('Usuario', null, {});
+  }
+};
