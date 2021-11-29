f61b559c076c48e9357f9f3b6d4b45e0e3f0a09b
JAVHM <55992367+JAVHM@users.noreply.github.com>
2021-11-29 03:18:20 -0500

    Notificaciones implementado en la DB

---
+++ backend/sequelize/models/notificacion.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Notificacion extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Favoritos.belongsTo(models.Usuario, {
+        foreignKey : 'id_u'
+      })
+    }
+  };
+  Notificacion.init({
+    id_u: DataTypes.INTEGER,
+    texto: DataTypes.STRING,
+    link: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Notificacion',
+    freezeTableName : true
+  });
+  return Notificacion;
+};
