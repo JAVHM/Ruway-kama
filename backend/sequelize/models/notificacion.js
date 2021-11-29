f40cf86a7d22665b80587ca39f79cddbbe270108
JAVHM <55992367+JAVHM@users.noreply.github.com>
2021-11-29 12:43:26 -0500

    fix notifcación modles

--- backend/sequelize/models/notificacion.js
+++ backend/sequelize/models/notificacion.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Favoritos.belongsTo(models.Usuario, {
+      Notificacion.belongsTo(models.Usuario, {
         foreignKey : 'id_u'
       })
     }
