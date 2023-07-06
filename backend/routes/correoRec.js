4b52a77f6778901b79401f6ad4d207002e2cd190
JAVHM <javanhmalpro@gmail.com>
2023-07-06 02:02:47 -0500

    Gracias Diego :D

--- backend/routes/correoRec.js
+++ backend/routes/correoRec.js
@@ -5,41 +5,40 @@ const { updateContrasena, getUsuarios } = require("../models/dao_usuario");
 
 router.get('/correoRec', (req, res) => {
 
-    res.render('correoRec')
-    res.redirect('/')
+    res.render('correoRec', {
+    })
 })
 router.post('/correoRec', async (req, res) => {
-    const correoUsuario = req.body.recuperar_email
-    const claveActual = req.body.recuperar_contrasena
-    const listaUsuarios = await getUsuarios()
-    const claveNueva = req.body.recuperar_nueva_contrasena
-    listaUsuarios.forEach(async (usuario) => {
-        //Encontrar al usuario con el mismo correo
-        if (correoUsuario == usuario.correo) {
-            //Se encontro al usuario con el correo
-            //Asi que se verifica la clave
-            console.log('Correo confirmado')
-            let compare = bcryptjs.compareSync(claveActual, usuario.contraseña)
-
-            console.log(compare)
-
-            if (compare) { //Booleano
-                //La clave es correcta
-                //Finalmente se guarda el usuario completo en la session
-                const claveNuevaEncriptada = await bcryptjs.hash(claveNueva, 8)
-                //console.log("req.session.u_id: ", req.session.u_id)
-                await updateContrasena(correoUsuario, claveNuevaEncriptada)
-                res.redirect('/')
-            } else {
-                //La clave es incorrecta
-                res.redirect('/errorFormulario')
-            }
-
+    const correoUsuario = req.body.recuperar_email;
+    const claveActual = req.body.recuperar_contrasena;
+    const listaUsuarios = await getUsuarios();
+    const claveNueva = req.body.recuperar_nueva_contrasena;
+  
+    let usuarioEncontrado = false;
+  
+    for (const usuario of listaUsuarios) {
+      if (correoUsuario === usuario.correo) {
+        usuarioEncontrado = true;
+        console.log('Correo confirmado');
+        const compare = bcryptjs.compareSync(claveActual, usuario.contraseña);
+        
+        console.log(compare);
+  
+        if (compare) {
+          const claveNuevaEncriptada = await bcryptjs.hash(claveNueva, 8);
+          await updateContrasena(correoUsuario, claveNuevaEncriptada);
+          res.redirect('/');
+          return; // Exit the function after sending the response
+        } else {
+          res.redirect('/errorFormulario');
+          return; // Exit the function after sending the response
         }
-    })
-    //Si sale del forEach, no se encontro a un usuario con el mismo correo
-    //Entones seria un caso de fallo.
-    res.redirect('/')
-})
+      }
+    }
+  
+    if (!usuarioEncontrado) {
+      res.redirect('/');
+    }
+  });
 
 module.exports = router;
